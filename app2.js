c33731befd61120240094fb835dafb0c24bc4911
Nash <jnash4@una.edu>
2022-10-13 13:16:54 -0500

    addFormFuntional

--- app2.js
+++ app2.js
@@ -4,22 +4,31 @@ const path = require('node:path')
 const bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const app = express()
-let posts = ''; 
-
 const PORT = process.env.PORT || 3000
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+let userName = ''; 
+let posts = ''; 
+
+
 async function main(){
     try {
         await client.connect();
-        databasesList = await client.db().admin().listDatabases();
-        console.log("Databases:");
-        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+        console.log("connected baby!")
+
+        //databasesList = await client.db().admin().listDatabases();
+        
+        //console.log("Databases:");
+        //databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+                
         posts = await client.db("NashPapa").collection("Papacis486").findOne();
         console.log(posts); 
         return posts; 
+
+        return "somehting"; 
     } catch (e) {
         console.error(e);
     } finally {
@@ -27,8 +36,20 @@ async function main(){
     }
 }
 app.get('/', async function (req, res) {
+    
     const result = await main().catch(console.error);
-    console.log("results: ", result.title);
-    res.send(`results:  ${ result.title }`);
+    console.log("results: ", result);
+    //res.send(`results:  ${ result }`);
+    // res.send(result.name); 
+    res.render('index', {
+        userName : result.name
+    })
 });
+
+app.post('/addinfo', async function (req, res) {
+    
+   //can you hardcode an database entyr.. 
+});
+
+
 app.listen(PORT, console.log(`server is running on port: ${PORT}` ));
